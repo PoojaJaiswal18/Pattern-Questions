361116c4ed4a11bb2c768304021aef31de906a5d
Pooja Jaiswal <jaiswal.pooja1809@gmail.com>
2023-04-08 10:41:25 +0530

    add solutions

---
+++ Q5.c
@@ -0,0 +1,35 @@
+ /* to print
+
+       * * * * *
+       * *   * *
+       *   *   *
+       * *   * *
+       * * * * *
+
+
+       */
+#include<stdio.h>
+int main()
+{
+    int  n,i,j,k;
+    printf("Enter the size of sq:");
+    scanf("%d",&n);
+
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(i==1||j==1||i==n||j==n || i==j|| j==((n-i)+1))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
